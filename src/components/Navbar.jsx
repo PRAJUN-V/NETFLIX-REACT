653fb0046a9e7d03a51df517a2f559ff42c568ab
prajun v <prajunvelandi@gmail.com>
2024-05-02 14:49:55 +0530

    authentication like signin signup using firebase completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,51 @@
 import React from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import { userAuth } from '../context/AuthContext'
+
 
 export const Navbar = () => {
+    const { user, logOut } = userAuth()
+    const navigate = useNavigate();
+    console.log(user);
+
+
+    const HandleLogout = async () => {
+        try {
+            await logOut();
+            navigate('/')
+        } catch (error) {
+            console.log(error);
+        }
+    }
     return (
-        <div className='flex items-center justify-between p-4 z-[100] absolute w-full'>
-            <h1 className='text-red-600 font-bold cursor-pointer text-4xl'>NETFLIX</h1>
-            <div>
-                <button className='text-white pr-4'>Sign In</button>
-                <button className='text-white bg-red-600 px-6 py-2 rounded cursor-pointer'>Sign Up</button>
-            </div>
+        <div className="flex items-center justify-between p-4 z-[100] w-full absolute">
+            <Link to="/">
+                <h1 className="text-red-600 text-4xl font-bold cursor-pointer">
+                    NETFLIX
+                </h1>
+                {/* <img className="w-44" src={logo} alt="" /> */}
+            </Link>
+            {user?.email ? (
+                <div>
+                    <Link to="/account">
+                        <button className="text-white pr-4">Account</button>
+                    </Link>
+                    <button onClick={HandleLogout} className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white ">
+                        Logout
+                    </button>
+                </div>
+            ) : (
+                <div>
+                    <Link to="/login">
+                        <button className="text-white pr-4">Sign In</button>
+                    </Link>
+                    <Link to="/signup">
+                        <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white ">
+                            Sign Up
+                        </button>
+                    </Link>
+                </div>
+            )}
         </div>
     )
 }
---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCfiOOnr5yLZtqghGfM38HDXjrEMqsOnu4",
+  authDomain: "netflix-react-a8364.firebaseapp.com",
+  projectId: "netflix-react-a8364",
+  storageBucket: "netflix-react-a8364.appspot.com",
+  messagingSenderId: "628467487202",
+  appId: "1:628467487202:web:fa3e5688c1778387f7e9bb",
+  measurementId: "G-JV895608TG"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth(app);
